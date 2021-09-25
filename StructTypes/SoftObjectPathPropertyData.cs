612fca22dc34b5839392c70ecdbae0f46b25c014
atenfyr <atenefyr@gmail.com>
2021-09-24 19:14:32 -0500

    SoftObjectPath & SoftClassPath have custom struct serialization

---
+++ StructTypes/SoftObjectPathPropertyData.cs
@@ -0,0 +1,68 @@
+﻿using System.IO;
+using UAssetAPI.PropertyTypes;
+
+namespace UAssetAPI.StructTypes
+{
+    public class SoftObjectPathPropertyData : PropertyData<FName>
+    {
+        public uint ID = 0;
+
+        public SoftObjectPathPropertyData(FName name, UAsset asset) : base(name, asset)
+        {
+
+        }
+
+        public SoftObjectPathPropertyData()
+        {
+
+        }
+
+        private static readonly FName CurrentPropertyType = new FName("SoftObjectPath");
+        public override bool HasCustomStructSerialization { get { return true; } }
+        public override FName PropertyType { get { return CurrentPropertyType; } }
+
+        public override void Read(BinaryReader reader, bool includeHeader, long leng1, long leng2 = 0)
+        {
+            if (includeHeader)
+            {
+                reader.ReadByte();
+            }
+
+            Value = reader.ReadFName(Asset);
+            ID = reader.ReadUInt32();
+        }
+
+        public override int Write(BinaryWriter writer, bool includeHeader)
+        {
+            if (includeHeader)
+            {
+                writer.Write((byte)0);
+            }
+
+            writer.WriteFName(Value, Asset);
+            writer.Write(ID);
+            return sizeof(int) * 3;
+        }
+
+        public override string ToString()
+        {
+            return "(" + Value.ToString() + ", " + ID + ")";
+        }
+
+        public override void FromString(string[] d)
+        {
+            FName output = FName.FromString(d[0]);
+            Asset.AddNameReference(output.Value);
+            Value = output;
+
+            if (uint.TryParse(d[1], out uint res2))
+            {
+                ID = res2;
+            }
+            else
+            {
+                ID = 0;
+            }
+        }
+    }
+}
