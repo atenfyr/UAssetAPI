163f31c7f958b0230b984ce4363b6d91fbd01ace
atenfyr <atenefyr@gmail.com>
2021-10-19 18:58:21 -0500

    FSmartName support

--- UAssetAPI/FName.cs
+++ UAssetAPI/FName.cs
@@ -87,13 +87,28 @@ namespace UAssetAPI
         /// <summary>Instance number.</summary>
         public int Number;
 
+        /// <summary>
+        /// Converts this FName instance into a human-readable string. This is the inverse of <see cref="FromString(string)"/>.
+        /// </summary>
+        /// <returns>The human-readable string that represents this FName.</returns>
+        /// <remarks>
+        /// The human-readable string is formatted as the string representation of the <see cref="Value"/> followed by the instance number <see cref="Number"/> in parentheses. In some special cases, the number in parentheses will be omitted, in which case it is safe to assume that it is zero.
+        /// </remarks>
         public override string ToString()
         {
             if (Number == int.MinValue) return Value.ToString();
             return Value.ToString() + "(" + Number + ")";
         }
 
-        /** Inverse of FName.ToString() */
+        /// <summary>
+        /// Converts a human-readable string into an FName instance. This is the inverse of <see cref="ToString"/>.
+        /// </summary>
+        /// <param name="val">The human-readable string to convert into an FName instance.</param>
+        /// <returns>An FName instance that this string represents.</returns>
+        /// <remarks>
+        /// If the string ends in a decimal number surrounded by parentheses, such as in alphabet(2), the number inside parentheses (2) will be used as the instance number and the rest of the string will be used as the value (alphabet).
+        /// Otherwise, the string itself will become the value of the new instance, and the instance number will default to zero.
+        /// </remarks>
         public static FName FromString(string val)
         {
             if (val == null || val == "null") return null;
@@ -137,6 +152,11 @@ namespace UAssetAPI
             return new FName((FString)Value.Clone(), Number);
         }
 
+        /// <summary>
+        /// Creates a new FName instance.
+        /// </summary>
+        /// <param name="value">The string literal that the new FName's value will be, verbatim.</param>
+        /// <param name="number">The instance number of the new FName.</param>
         public FName(string value, int number = 0)
         {
             if (value == null)
@@ -150,12 +170,20 @@ namespace UAssetAPI
             Number = number;
         }
 
+        /// <summary>
+        /// Creates a new FName instance.
+        /// </summary>
+        /// <param name="value">The FString that the FName's value will be, verbatim.</param>
+        /// <param name="number">The instance number of the new FName.</param>
         public FName(FString value, int number = 0)
         {
             Value = value;
             Number = number;
         }
 
+        /// <summary>
+        /// Creates a new blank FName instance.
+        /// </summary>
         public FName()
         {
             Value = new FString(string.Empty);
