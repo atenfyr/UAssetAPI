1a43738926c55cb52c77d63463bffc8c3ca90fb7
atenfyr <atenefyr@gmail.com>
2022-11-02 21:24:59 -0500

    StringAssetReference support

---
+++ UAssetAPI/PropertyTypes/Structs/StringAssetReferencePropertyData.cs
@@ -0,0 +1,24 @@
+﻿using UAssetAPI.UnrealTypes;
+
+namespace UAssetAPI.PropertyTypes.Structs
+{
+    /// <summary>
+    /// A struct that contains a string reference to a class. Can be used to make soft references to classes.
+    /// </summary>
+    public class StringAssetReferencePropertyData : SoftObjectPathPropertyData
+    {
+        public StringAssetReferencePropertyData(FName name) : base(name)
+        {
+
+        }
+
+        public StringAssetReferencePropertyData()
+        {
+
+        }
+
+        private static readonly FString CurrentPropertyType = new FString("StringAssetReference");
+        public override bool HasCustomStructSerialization { get { return true; } }
+        public override FString PropertyType { get { return CurrentPropertyType; } }
+    }
+}
