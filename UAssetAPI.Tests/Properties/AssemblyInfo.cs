d25ec881b976953a2dfba9cf11f6c142f454d8c6
atenfyr <atenefyr@gmail.com>
2022-11-08 23:43:33 -0600

    upgrade uassetapi tests csproj

--- UAssetAPI.Tests/Properties/AssemblyInfo.cs
+++ UAssetAPI.Tests/Properties/AssemblyInfo.cs
@@ -1,19 +1,6 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("UAssetAPI.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UAssetAPI.Tests")]
-[assembly: AssemblyCopyright("Copyright © Atenfyr 2022")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("cb796120-5ef8-4e9d-bcdb-d2f60a4efc69")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
