47ec19bc58bc27ea2cf39366e4e0fd62f3b499f0
spuds <71292624+bananaturtlesandwich@users.noreply.github.com>
2022-12-14 22:12:17 +0000

    inheriting to reduce code duplication

--- UAssetAPI/PropertyTypes/Objects/MulticastDelegatePropertyData.cs
+++ UAssetAPI/PropertyTypes/Objects/MulticastDelegatePropertyData.cs
@@ -85,7 +85,7 @@ namespace UAssetAPI.PropertyTypes.Objects
     /// <summary>
     /// Describes a list of functions bound to an Object.
     /// </summary>
-    public class MulticastSparseDelegatePropertyData : PropertyData<FDelegate[]>
+    public class MulticastSparseDelegatePropertyData : MulticastDelegatePropertyData
     {
         public MulticastSparseDelegatePropertyData(FName name) : base(name)
         {
@@ -99,72 +99,13 @@ namespace UAssetAPI.PropertyTypes.Objects
 
         private static readonly FString CurrentPropertyType = new FString("MulticastSparseDelegateProperty");
         public override FString PropertyType { get { return CurrentPropertyType; } }
-
-        public override void Read(AssetBinaryReader reader, FName parentName, bool includeHeader, long leng1, long leng2 = 0)
-        {
-            if (includeHeader)
-            {
-                PropertyGuid = reader.ReadPropertyGuid();
-            }
-
-            int numVals = reader.ReadInt32();
-            Value = new FDelegate[numVals];
-            for (int i = 0; i < numVals; i++)
-            {
-                Value[i] = new FDelegate(reader.XFER_OBJECT_POINTER(), reader.ReadFName());
-            }
-        }
-
-        public override int Write(AssetBinaryWriter writer, bool includeHeader)
-        {
-            if (includeHeader)
-            {
-                writer.WritePropertyGuid(PropertyGuid);
-            }
-
-            writer.Write(Value.Length);
-            for (int i = 0; i < Value.Length; i++)
-            {
-                writer.XFERPTR(Value[i].Object);
-                writer.Write(Value[i].Delegate);
-            }
-            return sizeof(int) + sizeof(int) * 3 * Value.Length;
-        }
-
-        public override string ToString()
-        {
-            string oup = "(";
-            for (int i = 0; i < Value.Length; i++)
-            {
-                oup += "(" + Convert.ToString(Value[i].Object.Index) + ", " + Value[i].Delegate.Value.Value + "), ";
-            }
-            return oup.Substring(0, oup.Length - 2) + ")";
-        }
-
-        public override void FromString(string[] d, UAsset asset)
-        {
-
-        }
-
-        protected override void HandleCloned(PropertyData res)
-        {
-            MulticastSparseDelegatePropertyData cloningProperty = (MulticastSparseDelegatePropertyData)res;
-
-            FDelegate[] newData = new FDelegate[Value.Length];
-            for (int i = 0; i < Value.Length; i++)
-            {
-                newData[i] = new FDelegate(Value[i].Object, (FName)Value[i].Delegate.Clone());
-            }
-
-            cloningProperty.Value = newData;
-        }
     }
 
 
     /// <summary>
     /// Describes a list of functions bound to an Object.
     /// </summary>
-    public class MulticastInlineDelegatePropertyData : PropertyData<FDelegate[]>
+    public class MulticastInlineDelegatePropertyData : MulticastDelegatePropertyData
     {
         public MulticastInlineDelegatePropertyData(FName name) : base(name)
         {
@@ -178,64 +119,5 @@ namespace UAssetAPI.PropertyTypes.Objects
 
         private static readonly FString CurrentPropertyType = new FString("MulticastInlineDelegateProperty");
         public override FString PropertyType { get { return CurrentPropertyType; } }
-
-        public override void Read(AssetBinaryReader reader, FName parentName, bool includeHeader, long leng1, long leng2 = 0)
-        {
-            if (includeHeader)
-            {
-                PropertyGuid = reader.ReadPropertyGuid();
-            }
-
-            int numVals = reader.ReadInt32();
-            Value = new FDelegate[numVals];
-            for (int i = 0; i < numVals; i++)
-            {
-                Value[i] = new FDelegate(reader.XFER_OBJECT_POINTER(), reader.ReadFName());
-            }
-        }
-
-        public override int Write(AssetBinaryWriter writer, bool includeHeader)
-        {
-            if (includeHeader)
-            {
-                writer.WritePropertyGuid(PropertyGuid);
-            }
-
-            writer.Write(Value.Length);
-            for (int i = 0; i < Value.Length; i++)
-            {
-                writer.XFERPTR(Value[i].Object);
-                writer.Write(Value[i].Delegate);
-            }
-            return sizeof(int) + sizeof(int) * 3 * Value.Length;
-        }
-
-        public override string ToString()
-        {
-            string oup = "(";
-            for (int i = 0; i < Value.Length; i++)
-            {
-                oup += "(" + Convert.ToString(Value[i].Object.Index) + ", " + Value[i].Delegate.Value.Value + "), ";
-            }
-            return oup.Substring(0, oup.Length - 2) + ")";
-        }
-
-        public override void FromString(string[] d, UAsset asset)
-        {
-
-        }
-
-        protected override void HandleCloned(PropertyData res)
-        {
-            MulticastInlineDelegatePropertyData cloningProperty = (MulticastInlineDelegatePropertyData)res;
-
-            FDelegate[] newData = new FDelegate[Value.Length];
-            for (int i = 0; i < Value.Length; i++)
-            {
-                newData[i] = new FDelegate(Value[i].Object, (FName)Value[i].Delegate.Clone());
-            }
-
-            cloningProperty.Value = newData;
-        }
     }
 }
