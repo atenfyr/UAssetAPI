bbbc1db610145c93f3c693c50564d91cb0afbd95
LongerWarrior <LongerWarrior@gmail.com>
2024-06-14 11:46:48 +0300

    Matrix and Vector4

---
+++ UAssetAPI/PropertyTypes/Structs/MatrixPropertyData.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using UAssetAPI.PropertyTypes.Objects;
+using UAssetAPI.UnrealTypes;
+using UAssetAPI.ExportTypes;
+
+namespace UAssetAPI.PropertyTypes.Structs
+{
+    /// <summary>
+    /// A vector in 3-D space composed of components (X, Y, Z) with floating point precision.
+    /// </summary>
+    public class MatrixPropertyData : PropertyData<FMatrix>
+    {
+        public MatrixPropertyData(FName name) : base(name)
+        {
+
+        }
+
+        public MatrixPropertyData()
+        {
+
+        }
+
+        private static readonly FString CurrentPropertyType = new FString("Matrix");
+        public override bool HasCustomStructSerialization { get { return true; } }
+        public override FString PropertyType { get { return CurrentPropertyType; } }
+
+        public override void Read(AssetBinaryReader reader, bool includeHeader, long leng1, long leng2 = 0)
+        {
+            if (includeHeader)
+            {
+                PropertyGuid = reader.ReadPropertyGuid();
+            }
+
+            Value = new FMatrix(reader);
+        }
+
+        public override int Write(AssetBinaryWriter writer, bool includeHeader)
+        {
+            if (includeHeader)
+            {
+                writer.WritePropertyGuid(PropertyGuid);
+            }
+
+            return Value.Write(writer);
+        }
+
+        //public override void FromString(string[] d, UAsset asset)
+        //{
+        //    double.TryParse(d[0], out double X);
+        //    double.TryParse(d[1], out double Y);
+        //    double.TryParse(d[2], out double Z);
+        //    Value = new FVector(X, Y, Z);
+        //}
+
+        //public override string ToString()
+        //{
+        //    return "(" + Value.X + ", " + Value.Y + ", " + Value.Z + ")";
+        //}
+    }
+}
