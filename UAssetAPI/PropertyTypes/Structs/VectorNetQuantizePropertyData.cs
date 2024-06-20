83dd19ef24436b0d066679ce5a11b7dc642ff899
LongerWarrior <LongerWarrior@gmail.com>
2024-06-20 23:18:17 +0300

    Vector_NetQuantize type

---
+++ UAssetAPI/PropertyTypes/Structs/VectorNetQuantizePropertyData.cs
@@ -0,0 +1,47 @@
+﻿using UAssetAPI.UnrealTypes;
+
+namespace UAssetAPI.PropertyTypes.Structs;
+
+public class VectorNetQuantizePropertyData : VectorPropertyData
+{
+    public VectorNetQuantizePropertyData(FName name) : base(name) { }
+
+    public VectorNetQuantizePropertyData() { }
+
+    private static readonly FString CurrentPropertyType = new FString("Vector_NetQuantize");
+    public override bool HasCustomStructSerialization => true;
+    public override FString PropertyType => CurrentPropertyType;
+}
+
+public class VectorNetQuantizeNormalPropertyData : VectorPropertyData
+{
+    public VectorNetQuantizeNormalPropertyData(FName name) : base(name) { }
+
+    public VectorNetQuantizeNormalPropertyData() { }
+
+    private static readonly FString CurrentPropertyType = new FString("Vector_NetQuantizeNormal");
+    public override bool HasCustomStructSerialization => true;
+    public override FString PropertyType => CurrentPropertyType;
+}
+
+public class VectorNetQuantize10PropertyData : VectorPropertyData
+{
+    public VectorNetQuantize10PropertyData(FName name) : base(name) { }
+
+    public VectorNetQuantize10PropertyData() { }
+
+    private static readonly FString CurrentPropertyType = new FString("Vector_NetQuantize10");
+    public override bool HasCustomStructSerialization => true;
+    public override FString PropertyType => CurrentPropertyType;
+}
+
+public class VectorNetQuantize100PropertyData : VectorPropertyData
+{
+    public VectorNetQuantize100PropertyData(FName name) : base(name) { }
+
+    public VectorNetQuantize100PropertyData() { }
+
+    private static readonly FString CurrentPropertyType = new FString("Vector_NetQuantize100");
+    public override bool HasCustomStructSerialization => true;
+    public override FString PropertyType => CurrentPropertyType;
+}
