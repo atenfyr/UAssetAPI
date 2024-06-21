35d7eb90c88ec60215ecb129ee4ce2869b99877a
LongerWarrior <LongerWarrior@gmail.com>
2024-06-22 01:32:48 +0300

    Vector3f and ClothTeherData property

---
+++ UAssetAPI/PropertyTypes/Structs/ClothTetherDataPropertyData.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using UAssetAPI.PropertyTypes.Objects;
+using UAssetAPI.UnrealTypes;
+
+namespace UAssetAPI.PropertyTypes.Structs;
+
+/// <summary>
+/// Long range attachment tether pathfinding based on Dijkstra's algorithm.
+/// </summary>
+public class ClothTetherDataPropertyData : StructPropertyData
+{
+    public (int, int, float)[][] Tethers;
+
+    public ClothTetherDataPropertyData(FName name) : base(name)
+    {
+        Value = new List<PropertyData>();
+    }
+
+    public ClothTetherDataPropertyData() { }
+
+    private static readonly FString CurrentPropertyType = new FString("ClothTetherData");
+    public override bool HasCustomStructSerialization => true;
+    public override FString PropertyType => CurrentPropertyType;
+    
+
+    public override void Read(AssetBinaryReader reader, bool includeHeader, long leng1, long leng2 = 0, PropertySerializationContext serializationContext = PropertySerializationContext.Normal)
+    {
+        if (includeHeader)
+        {
+            PropertyGuid = reader.ReadPropertyGuid();
+        }
+
+        StructType = FName.DefineDummy(reader.Asset, CurrentPropertyType);
+        base.Read(reader, false, 1, 0, PropertySerializationContext.StructFallback);
+
+        int numElements = reader.ReadInt32();
+        Tethers = new (int, int, float)[numElements][];
+        for (int i = 0; i < numElements; i++)
+        {
+            int numInnerElements = reader.ReadInt32();
+            Tethers[i] = new (int, int, float)[numInnerElements];
+            for (int j = 0; j < numInnerElements; j++)
+            {
+                Tethers[i][j] = (reader.ReadInt32(), reader.ReadInt32(), reader.ReadSingle());
+            }
+        }
+    }
+
+    public override int Write(AssetBinaryWriter writer, bool includeHeader, PropertySerializationContext serializationContext = PropertySerializationContext.Normal)
+    {
+        if (includeHeader)
+        {
+            writer.WritePropertyGuid(PropertyGuid);
+        }
+
+        StructType = FName.DefineDummy(writer.Asset, CurrentPropertyType);
+        int totalSize = base.Write(writer, includeHeader, PropertySerializationContext.StructFallback);
+
+        writer.Write(Tethers.Length);
+        totalSize += sizeof(int);
+        for (int i = 0; i < Tethers.Length; i++)
+        {
+            writer.Write(Tethers[i].Length);
+            totalSize += sizeof(int);
+            for (int j = 0; j < Tethers[i].Length; j++)
+            {
+                writer.Write(Tethers[i][j].Item1);
+                writer.Write(Tethers[i][j].Item2);
+                writer.Write(Tethers[i][j].Item3);
+                totalSize += sizeof(int) * 2 + sizeof(float);
+            }
+        }
+
+        return totalSize;
+    }
+}
