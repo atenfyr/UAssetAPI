94b6c86db461e1c5c31168450cd1f50a8a63dd59
LongerWarrior <LongerWarrior@gmail.com>
2024-06-25 22:37:38 +0300

    fixng vector properties

--- UAssetAPI/PropertyTypes/Structs/VectorNetQuantizePropertyData.cs
+++ UAssetAPI/PropertyTypes/Structs/VectorNetQuantizePropertyData.cs
@@ -1,19 +1,63 @@
-﻿using UAssetAPI.UnrealTypes;
+﻿using System;
+using UAssetAPI.PropertyTypes.Objects;
+using UAssetAPI.UnrealTypes;
 
 namespace UAssetAPI.PropertyTypes.Structs;
 
-public class VectorNetQuantizePropertyData : VectorPropertyData
+public class VectorNetQuantizePropertyData : StructPropertyData
 {
-    public VectorNetQuantizePropertyData(FName name) : base(name) { }
+    public VectorNetQuantizePropertyData(FName name, FName forcedType) : base(name, forcedType)
+    {
+        Value.Add(new VectorPropertyData(name)); //backup for isZero
+    }
+
+    public VectorNetQuantizePropertyData(FName name) : base(name)
+    {
+        Value.Add(new VectorPropertyData(name));
+    }
 
     public VectorNetQuantizePropertyData() { }
 
     private static readonly FString CurrentPropertyType = new FString("Vector_NetQuantize");
     public override bool HasCustomStructSerialization => true;
     public override FString PropertyType => CurrentPropertyType;
+
+    public override void Read(AssetBinaryReader reader, bool includeHeader, long leng1, long leng2 = 0, PropertySerializationContext serializationContext = PropertySerializationContext.Normal)
+    {
+        Value = [];
+        //either this or 5.0+ or unversioned
+        if (reader.Asset.ObjectVersionUE5 >= ObjectVersionUE5.LARGE_WORLD_COORDINATES)
+        {
+            var data = new VectorPropertyData(Name);
+            data.Read(reader, includeHeader, leng1, leng2, serializationContext);
+            Value.Add(data);
+        }
+        else
+        {
+            StructType = FName.DefineDummy(reader.Asset, PropertyType);
+            base.Read(reader, includeHeader, 1, leng2, PropertySerializationContext.StructFallback);
+        }
+    }
+
+    public override int Write(AssetBinaryWriter writer, bool includeHeader, PropertySerializationContext serializationContext = PropertySerializationContext.Normal)
+    {
+        if (writer.Asset.ObjectVersionUE5 >= ObjectVersionUE5.LARGE_WORLD_COORDINATES)
+        {
+            if (Value.Count == 1 && Value[0] is VectorPropertyData vector)
+            {
+                return Value[0].Write(writer, includeHeader, serializationContext);
+            }
+            throw new FormatException($"{PropertyType} must have a VectorPropertyData child");
+        }
+        else
+        {
+            StructType = FName.DefineDummy(writer.Asset, PropertyType);
+            return base.Write(writer, includeHeader, PropertySerializationContext.StructFallback);
+        }
+    }
 }
 
-public class VectorNetQuantizeNormalPropertyData : VectorPropertyData
+public class VectorNetQuantizeNormalPropertyData : VectorNetQuantizePropertyData
 {
     public VectorNetQuantizeNormalPropertyData(FName name) : base(name) { }
 
@@ -24,7 +68,7 @@ public class VectorNetQuantizeNormalPropertyData : VectorPropertyData
     public override FString PropertyType => CurrentPropertyType;
 }
 
-public class VectorNetQuantize10PropertyData : VectorPropertyData
+public class VectorNetQuantize10PropertyData : VectorNetQuantizePropertyData
 {
     public VectorNetQuantize10PropertyData(FName name) : base(name) { }
 
@@ -35,7 +79,7 @@ public class VectorNetQuantize10PropertyData : VectorPropertyData
     public override FString PropertyType => CurrentPropertyType;
 }
 
-public class VectorNetQuantize100PropertyData : VectorPropertyData
+public class VectorNetQuantize100PropertyData : VectorNetQuantizePropertyData
 {
     public VectorNetQuantize100PropertyData(FName name) : base(name) { }
 
--- UAssetAPI/PropertyTypes/Structs/VectorPropertyData.cs
+++ UAssetAPI/PropertyTypes/Structs/VectorPropertyData.cs
@@ -1,82 +1,51 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.IO;
-using UAssetAPI.PropertyTypes.Objects;
+﻿using UAssetAPI.PropertyTypes.Objects;
 using UAssetAPI.UnrealTypes;
-using UAssetAPI.ExportTypes;
 
-namespace UAssetAPI.PropertyTypes.Structs
+namespace UAssetAPI.PropertyTypes.Structs;
+
+/// <summary>
+/// A vector in 3-D space composed of components (X, Y, Z) with floating point precision.
+/// </summary>
+public class VectorPropertyData : PropertyData<FVector>
 {
-    /// <summary>
-    /// A vector in 3-D space composed of components (X, Y, Z) with floating point precision.
-    /// </summary>
-    public class VectorPropertyData : PropertyData<FVector>
+    public VectorPropertyData(FName name) : base(name) { }
+
+    public VectorPropertyData() { }
+
+    private static readonly FString CurrentPropertyType = new FString("Vector");
+    public override bool HasCustomStructSerialization => true;
+    public override FString PropertyType => CurrentPropertyType;
+
+    public override void Read(AssetBinaryReader reader, bool includeHeader, long leng1, long leng2 = 0, PropertySerializationContext serializationContext = PropertySerializationContext.Normal)
     {
-        public VectorPropertyData(FName name) : base(name)
+        if (includeHeader)
         {
-
+            PropertyGuid = reader.ReadPropertyGuid();
         }
 
-        public VectorPropertyData()
-        {
+        Value = new FVector(reader);
+    }
 
+    public override int Write(AssetBinaryWriter writer, bool includeHeader, PropertySerializationContext serializationContext = PropertySerializationContext.Normal)
+    {
+        if (includeHeader)
+        {
+            writer.WritePropertyGuid(PropertyGuid);
         }
 
-        private static readonly FString CurrentPropertyType = new FString("Vector");
-        public override bool HasCustomStructSerialization { get { return true; } }
-        public override FString PropertyType { get { return CurrentPropertyType; } }
+        return Value.Write(writer);
+    }
 
-        public override void Read(AssetBinaryReader reader, bool includeHeader, long leng1, long leng2 = 0, PropertySerializationContext serializationContext = PropertySerializationContext.Normal)
-        {
-            if (includeHeader)
-            {
-                PropertyGuid = reader.ReadPropertyGuid();
-            }
+    public override void FromString(string[] d, UAsset asset)
+    {
+        double.TryParse(d[0], out double X);
+        double.TryParse(d[1], out double Y);
+        double.TryParse(d[2], out double Z);
+        Value = new FVector(X, Y, Z);
+    }
 
-            if (reader.Asset.ObjectVersionUE5 >= ObjectVersionUE5.LARGE_WORLD_COORDINATES)
-            {
-                Value = new FVector(reader.ReadDouble(), reader.ReadDouble(), reader.ReadDouble());
-            }
-            else
-            {
-                Value = new FVector(reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle());
-            }
-        }
-
-        public override int Write(AssetBinaryWriter writer, bool includeHeader, PropertySerializationContext serializationContext = PropertySerializationContext.Normal)
-        {
-            if (includeHeader)
-            {
-                writer.WritePropertyGuid(PropertyGuid);
-            }
-
-            if (writer.Asset.ObjectVersionUE5 >= ObjectVersionUE5.LARGE_WORLD_COORDINATES)
-            {
-                writer.Write(Value.X);
-                writer.Write(Value.Y);
-                writer.Write(Value.Z);
-                return sizeof(double) * 3;
-            }
-            else
-            {
-                writer.Write(Value.XFloat);
-                writer.Write(Value.YFloat);
-                writer.Write(Value.ZFloat);
-                return sizeof(float) * 3;
-            }
-        }
-
-        public override void FromString(string[] d, UAsset asset)
-        {
-            double.TryParse(d[0], out double X);
-            double.TryParse(d[1], out double Y);
-            double.TryParse(d[2], out double Z);
-            Value = new FVector(X, Y, Z);
-        }
-
-        public override string ToString()
-        {
-            return "(" + Value.X + ", " + Value.Y + ", " + Value.Z + ")";
-        }
+    public override string ToString()
+    {
+        return "(" + Value.X + ", " + Value.Y + ", " + Value.Z + ")";
     }
 }
