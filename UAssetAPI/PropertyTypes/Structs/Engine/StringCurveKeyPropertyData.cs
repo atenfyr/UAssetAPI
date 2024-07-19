a5c0a9eeb557f50976c8008e8f2237a8c9428419
LongerWarrior <LongerWarrior@gmail.com>
2024-07-19 23:48:12 +0300

    StringCurveKeyPropertyData

---
+++ UAssetAPI/PropertyTypes/Structs/Engine/StringCurveKeyPropertyData.cs
@@ -0,0 +1,31 @@
+﻿using UAssetAPI.PropertyTypes.Objects;
+using UAssetAPI.UnrealTypes;
+
+namespace UAssetAPI.PropertyTypes.Structs;
+
+public struct FStringCurveKey(AssetBinaryReader reader) : IStruct<FStringCurveKey>
+{
+    public float Time = reader.ReadSingle();
+    public FString Value = reader.ReadFString();
+
+    public static FStringCurveKey Read(AssetBinaryReader reader) => new FStringCurveKey(reader);
+
+    public int Write(AssetBinaryWriter writer)
+    {
+        var offset = writer.BaseStream.Position;
+
+        writer.Write(Time);
+        writer.Write(Value);
+
+        return (int)(writer.BaseStream.Position - offset);
+    }
+}
+
+public class StringCurveKeyPropertyData : BasePropertyData<FStringCurveKey>
+{
+    public StringCurveKeyPropertyData(FName name) : base(name) { }
+    public StringCurveKeyPropertyData() { }
+
+    private static readonly FString CurrentPropertyType = new FString("StringCurveKey");
+    public override FString PropertyType => CurrentPropertyType;
+}
