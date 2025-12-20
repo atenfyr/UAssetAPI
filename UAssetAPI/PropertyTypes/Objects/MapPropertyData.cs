atenfyr/UAssetAPI

commit 5f715e711735bacae7789ca1ee1e2a1e64620380

Full PropertyTypeName support for 5.4+ tagged properties Some PropertyData refactoring

UAssetAPI/PropertyTypes/Objects/MapPropertyData.cs
@@ -1,190 +1,202 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
+using System;
 using System.Linq;
 using UAssetAPI.JSON;
 using UAssetAPI.PropertyTypes.Structs;
 using UAssetAPI.UnrealTypes;
 using UAssetAPI.Unversioned;
 
-namespace UAssetAPI.PropertyTypes.Objects
+namespace UAssetAPI.PropertyTypes.Objects;
+
+/// <summary>
+/// Describes a map.
+/// </summary>
+public class MapPropertyData : PropertyData
 {
     /// <summary>
-    /// Describes a map.
+    /// The map that this property represents.
     /// </summary>
-    public class MapPropertyData : PropertyData
+    [JsonProperty]
+    [JsonConverter(typeof(TMapJsonConverter<PropertyData, PropertyData>))]
+    public TMap<PropertyData, PropertyData> Value;
+
+    /// <summary>
+    /// Used when the length of the map is zero.
+    /// </summary>]
+    [JsonProperty]
+    public FName KeyType;
+
+    /// <summary>
+    /// Used when the length of the map is zero.
+    /// </summary>
+    [JsonProperty]
+    public FName ValueType;
+
+    public bool ShouldSerializeKeyType() => Value.Count == 0;
+
+    public bool ShouldSerializeValueType() => Value.Count == 0;
+
+    [JsonProperty]
+    public PropertyData[] KeysToRemove = null;
+
+    public MapPropertyData(FName name) : base(name)
     {
-        /// <summary>
-        /// The map that this property represents.
-        /// </summary>
-        [JsonProperty]
-        [JsonConverter(typeof(TMapJsonConverter<PropertyData, PropertyData>))]
-        public TMap<PropertyData, PropertyData> Value;
+        Value = new TMap<PropertyData, PropertyData>();
+    }
 
-        /// <summary>
-        /// Used when the length of the map is zero.
-        /// </summary>]
-        [JsonProperty]
-        public FName KeyType;
+    public MapPropertyData()
+    {
+        Value = new TMap<PropertyData, PropertyData>();
+    }
 
-        /// <summary>
-        /// Used when the length of the map is zero.
-        /// </summary>
-        [JsonProperty]
-        public FName ValueType;
+    private static readonly FString CurrentPropertyType = new FString("MapProperty");
+    public override FString PropertyType { get { return CurrentPropertyType; } }
 
-        public bool ShouldSerializeKeyType()
+    private PropertyData MapTypeToClass(FName type, FName name, AssetBinaryReader reader, int leng, bool includeHeader, bool isKey)
+    {
+        switch (type.Value.Value)
         {
-            return Value.Count == 0;
-        }
-
-        public bool ShouldSerializeValueType()
-        {
-            return Value.Count == 0;
-        }
-
-        [JsonProperty]
-        public PropertyData[] KeysToRemove = null;
-
-        public MapPropertyData(FName name) : base(name)
-        {
-            Value = new TMap<PropertyData, PropertyData>();
-        }
-
-        public MapPropertyData()
-        {
-            Value = new TMap<PropertyData, PropertyData>();
-        }
-
-        private static readonly FString CurrentPropertyType = new FString("MapProperty");
-        public override FString PropertyType { get { return CurrentPropertyType; } }
-
-        private PropertyData MapTypeToClass(FName type, FName name, AssetBinaryReader reader, int leng, bool includeHeader, bool isKey)
-        {
-            switch (type.Value.Value)
-            {
-                case "StructProperty":
-                    FName strucType = null;
-
-                    if (reader.Asset.Mappings != null && reader.Asset.Mappings.TryGetPropertyData(Name, Ancestry, reader.Asset, out UsmapMapData mapDat))
+            case "StructProperty":
+                FName strucType = null;
+                FPropertyTypeName propertyTypeNameLocal = PropertyTypeName?.GetParameter(isKey ? 0 : 1);
+                if (reader.Asset.ObjectVersionUE5 >= ObjectVersionUE5.PROPERTY_TAG_COMPLETE_TYPE_NAME)
+                {
+                    strucType = propertyTypeNameLocal?.GetParameter(0).GetName();
+                }
+                else if (reader.Asset.Mappings != null && reader.Asset.Mappings.TryGetPropertyData(Name, Ancestry, reader.Asset, out UsmapMapData mapDat))
+                {
+                    if (isKey && mapDat.InnerType is UsmapStructData strucDat1)
                     {
-                        if (isKey && mapDat.InnerType is UsmapStructData strucDat1)
-                        {
-                            strucType = FName.DefineDummy(reader.Asset, strucDat1.StructType);
-                        }
-                        else if (mapDat.ValueType is UsmapStructData strucDat2)
-                        {
-                            strucType = FName.DefineDummy(reader.Asset, strucDat2.StructType);
-                        }
+                        strucType = FName.DefineDummy(reader.Asset, strucDat1.StructType);
                     }
-                    else if (reader.Asset.MapStructTypeOverride.ContainsKey(name.Value.Value))
+                    else if (mapDat.ValueType is UsmapStructData strucDat2)
                     {
-                        if (isKey)
-                        {
-                            strucType = FName.DefineDummy(reader.Asset, reader.Asset.MapStructTypeOverride[name.Value.Value].Item1);
-                        }
-                        else
-                        {
-                            strucType = FName.DefineDummy(reader.Asset, reader.Asset.MapStructTypeOverride[name.Value.Value].Item2);
-                            if (name.Value.Value == "TrackSignatureToTrackIdentifier" && reader.Asset.GetEngineVersion() <= EngineVersion.VER_UE4_18)
-                                strucType = FName.DefineDummy(reader.Asset, "Generic");
-                        }
+                        strucType = FName.DefineDummy(reader.Asset, strucDat2.StructType);
                     }
+                }
+                else if (reader.Asset.MapStructTypeOverride.ContainsKey(name.Value.Value))
+                {
+                    if (isKey)
+                    {
+                        strucType = FName.DefineDummy(reader.Asset, reader.Asset.MapStructTypeOverride[name.Value.Value].Item1);
+                    }
+                    else
+                    {
+                        strucType = FName.DefineDummy(reader.Asset, reader.Asset.MapStructTypeOverride[name.Value.Value].Item2);
+                        if (name.Value.Value == "TrackSignatureToTrackIdentifier" && reader.Asset.GetEngineVersion() <= EngineVersion.VER_UE4_18)
+                            strucType = FName.DefineDummy(reader.Asset, "Generic");
+                    }
+                }
 
-                    if (strucType?.Value == null) strucType = FName.DefineDummy(reader.Asset, "Generic");
+                if (strucType?.Value == null) strucType = FName.DefineDummy(reader.Asset, "Generic");
 
-                    StructPropertyData data = new StructPropertyData(name, strucType);
-                    data.Ancestry.Initialize(Ancestry, Name);
-                    data.Offset = reader.BaseStream.Position;
-                    data.Read(reader, false, 1, 0, PropertySerializationContext.Map);
-                    return data;
-                default:
-                    var res = MainSerializer.TypeToClass(type, name, Ancestry, Name, null, reader.Asset, null, leng);
-                    res.Ancestry.Initialize(Ancestry, Name);
-                    res.Read(reader, includeHeader, leng, 0, PropertySerializationContext.Map);
-                    return res;
-            }
+                StructPropertyData data = new StructPropertyData(name, strucType);
+                data.Ancestry.Initialize(Ancestry, Name);
+                data.Offset = reader.BaseStream.Position;
+                data.PropertyTypeName = propertyTypeNameLocal;
+                data.Read(reader, false, 1, 0, PropertySerializationContext.Map);
+                return data;
+            default:
+                var res = MainSerializer.TypeToClass(type, name, Ancestry, Name, null, reader.Asset, null, leng, propertyTypeName: PropertyTypeName?.GetParameter(0));
+                res.Ancestry.Initialize(Ancestry, Name);
+                res.Read(reader, includeHeader, leng, 0, PropertySerializationContext.Map);
+                return res;
+        }
+    }
+
+    private TMap<PropertyData, PropertyData> ReadRawMap(AssetBinaryReader reader, FName type1, FName type2, int numEntries)
+    {
+        var resultingDict = new TMap<PropertyData, PropertyData>();
+
+        PropertyData data1 = null;
+        PropertyData data2 = null;
+        for (int i = 0; i < numEntries; i++)
+        {
+            data1 = MapTypeToClass(type1, Name, reader, 0, false, true);
+            data2 = MapTypeToClass(type2, Name, reader, 0, false, false);
+
+            resultingDict.Add(data1, data2);
         }
 
-        private TMap<PropertyData, PropertyData> ReadRawMap(AssetBinaryReader reader, FName type1, FName type2, int numEntries)
-        {
-            var resultingDict = new TMap<PropertyData, PropertyData>();
+        return resultingDict;
+    }
 
-            PropertyData data1 = null;
-            PropertyData data2 = null;
-            for (int i = 0; i < numEntries; i++)
+    public override void Read(AssetBinaryReader reader, bool includeHeader, long leng1, long leng2 = 0, PropertySerializationContext serializationContext = PropertySerializationContext.Normal)
+    {
+        FName type1 = null, type2 = null;
+        if (includeHeader && !reader.Asset.HasUnversionedProperties)
+        {
+            if (reader.Asset.ObjectVersionUE5 >= ObjectVersionUE5.PROPERTY_TAG_COMPLETE_TYPE_NAME)
             {
-                data1 = MapTypeToClass(type1, Name, reader, 0, false, true);
-                data2 = MapTypeToClass(type2, Name, reader, 0, false, false);
-
-                resultingDict.Add(data1, data2);
+                if (PropertyTypeName is null) throw new FormatException("PropertyTypeName is required to read MapProperty with complete type names.");
+                type1 = PropertyTypeName.GetParameter(0).GetName();
+                type2 = PropertyTypeName.GetParameter(1).GetName();
             }
-
-            return resultingDict;
-        }
-
-        public override void Read(AssetBinaryReader reader, bool includeHeader, long leng1, long leng2 = 0, PropertySerializationContext serializationContext = PropertySerializationContext.Normal)
-        {
-            FName type1 = null, type2 = null;
-            if (includeHeader && !reader.Asset.HasUnversionedProperties)
+            else
             {
                 type1 = reader.ReadFName();
                 type2 = reader.ReadFName();
-                this.ReadEndPropertyTag(reader);
             }
 
-            if (reader.Asset.Mappings != null && type1 == null && type2 == null && reader.Asset.Mappings.TryGetPropertyData(Name, Ancestry, reader.Asset, out UsmapMapData strucDat1))
-            {
-                type1 = FName.DefineDummy(reader.Asset, strucDat1.InnerType.Type.ToString());
-                type2 = FName.DefineDummy(reader.Asset, strucDat1.ValueType.Type.ToString());
-            }
-
-            int numKeysToRemove = reader.ReadInt32();
-            KeysToRemove = new PropertyData[numKeysToRemove];
-            for (int i = 0; i < numKeysToRemove; i++)
-            {
-                KeysToRemove[i] = MapTypeToClass(type1, Name, reader, 0, false, true);
-            }
-
-            int numEntries = reader.ReadInt32();
-            if (numEntries == 0)
-            {
-                KeyType = type1;
-                ValueType = type2;
-            }
-
-            Value = ReadRawMap(reader, type1, type2, numEntries);
+            this.ReadEndPropertyTag(reader);
         }
 
-        public override void ResolveAncestries(UAsset asset, AncestryInfo ancestrySoFar)
+        if (reader.Asset.Mappings != null && type1 == null && type2 == null && reader.Asset.Mappings.TryGetPropertyData(Name, Ancestry, reader.Asset, out UsmapMapData strucDat1))
         {
-            var ancestryNew = (AncestryInfo)ancestrySoFar.Clone();
-            ancestryNew.SetAsParent(Name);
-
-            if (Value != null)
-            {
-                foreach (var entry in Value)
-                {
-                    entry.Key.ResolveAncestries(asset, ancestryNew);
-                    entry.Value.ResolveAncestries(asset, ancestryNew);
-                }
-            }
-            base.ResolveAncestries(asset, ancestrySoFar);
+            type1 = FName.DefineDummy(reader.Asset, strucDat1.InnerType.Type.ToString());
+            type2 = FName.DefineDummy(reader.Asset, strucDat1.ValueType.Type.ToString());
         }
 
-        private void WriteRawMap(AssetBinaryWriter writer, TMap<PropertyData, PropertyData> map)
+        int numKeysToRemove = reader.ReadInt32();
+        KeysToRemove = new PropertyData[numKeysToRemove];
+        for (int i = 0; i < numKeysToRemove; i++)
         {
-            if (map == null) return;
-            foreach (var entry in map)
-            {
-                entry.Key.Offset = writer.BaseStream.Position;
-                entry.Key.Write(writer, false, PropertySerializationContext.Map);
-                entry.Value.Offset = writer.BaseStream.Position;
-                entry.Value.Write(writer, false, PropertySerializationContext.Map);
-            }
+            KeysToRemove[i] = MapTypeToClass(type1, Name, reader, 0, false, true);
         }
 
-        public override int Write(AssetBinaryWriter writer, bool includeHeader, PropertySerializationContext serializationContext = PropertySerializationContext.Normal)
+        int numEntries = reader.ReadInt32();
+        if (numEntries == 0)
         {
-            if (includeHeader && !writer.Asset.HasUnversionedProperties)
+            KeyType = type1;
+            ValueType = type2;
+        }
+
+        Value = ReadRawMap(reader, type1, type2, numEntries);
+    }
+
+    public override void ResolveAncestries(UAsset asset, AncestryInfo ancestrySoFar)
+    {
+        var ancestryNew = (AncestryInfo)ancestrySoFar.Clone();
+        ancestryNew.SetAsParent(Name);
+
+        if (Value != null)
+        {
+            foreach (var entry in Value)
+            {
+                entry.Key.ResolveAncestries(asset, ancestryNew);
+                entry.Value.ResolveAncestries(asset, ancestryNew);
+            }
+        }
+        base.ResolveAncestries(asset, ancestrySoFar);
+    }
+
+    private void WriteRawMap(AssetBinaryWriter writer, TMap<PropertyData, PropertyData> map)
+    {
+        if (map == null) return;
+        foreach (var entry in map)
+        {
+            entry.Key.Offset = writer.BaseStream.Position;
+            entry.Key.Write(writer, false, PropertySerializationContext.Map);
+            entry.Value.Offset = writer.BaseStream.Position;
+            entry.Value.Write(writer, false, PropertySerializationContext.Map);
+        }
+    }
+
+    public override int Write(AssetBinaryWriter writer, bool includeHeader, PropertySerializationContext serializationContext = PropertySerializationContext.Normal)
+    {
+        if (includeHeader && !writer.Asset.HasUnversionedProperties)
+        {
+            if (writer.Asset.ObjectVersionUE5 < ObjectVersionUE5.PROPERTY_TAG_COMPLETE_TYPE_NAME)
             {
                 if (Value.Count > 0)
                 {
@@ -196,48 +208,47 @@ namespace UAssetAPI.PropertyTypes.Objects
                     writer.Write(KeyType);
                     writer.Write(ValueType);
                 }
-                this.WriteEndPropertyTag(writer);
             }
-
-            int here = (int)writer.BaseStream.Position;
-            writer.Write(KeysToRemove?.Length ?? 0);
-            if (KeysToRemove != null)
-            {
-                for (int i = 0; i < KeysToRemove.Length; i++)
-                {
-                    var entry = KeysToRemove[i];
-                    entry.Offset = writer.BaseStream.Position;
-                    entry.Write(writer, false, PropertySerializationContext.Array);
-                }
-            }
-
-            writer.Write(Value?.Count ?? 0);
-            WriteRawMap(writer, Value);
-            return (int)writer.BaseStream.Position - here;
+            this.WriteEndPropertyTag(writer);
         }
 
-        protected override void HandleCloned(PropertyData res)
+        int here = (int)writer.BaseStream.Position;
+        writer.Write(KeysToRemove?.Length ?? 0);
+        if (KeysToRemove != null)
         {
-            MapPropertyData cloningProperty = (MapPropertyData)res;
-
-            if (this.Value != null)
+            for (int i = 0; i < KeysToRemove.Length; i++)
             {
-                var newDict = new TMap<PropertyData, PropertyData>();
-                foreach (var entry in this.Value)
-                {
-                    newDict[(PropertyData)entry.Key.Clone()] = (PropertyData)entry.Value.Clone();
-                }
-                cloningProperty.Value = newDict;
+                var entry = KeysToRemove[i];
+                entry.Offset = writer.BaseStream.Position;
+                entry.Write(writer, false, PropertySerializationContext.Array);
             }
-            else
-            {
-                cloningProperty.Value = null;
-            }
-
-            cloningProperty.KeysToRemove = (PropertyData[])this.KeysToRemove?.Clone();
-
-            cloningProperty.KeyType = (FName)this.KeyType?.Clone();
-            cloningProperty.ValueType = (FName)this.ValueType?.Clone();
         }
+
+        writer.Write(Value?.Count ?? 0);
+        WriteRawMap(writer, Value);
+        return (int)writer.BaseStream.Position - here;
+    }
+
+    protected override void HandleCloned(PropertyData res)
+    {
+        MapPropertyData cloningProperty = (MapPropertyData)res;
+
+        if (this.Value != null)
+        {
+            var newDict = new TMap<PropertyData, PropertyData>();
+            foreach (var entry in this.Value)
+            {
+                newDict[(PropertyData)entry.Key.Clone()] = (PropertyData)entry.Value.Clone();
+            }
+            cloningProperty.Value = newDict;
+        }
+        else
+        {
+            cloningProperty.Value = null;
+        }
+
+        cloningProperty.KeysToRemove = (PropertyData[])this.KeysToRemove?.Clone();
+        cloningProperty.KeyType = (FName)this.KeyType?.Clone();
+        cloningProperty.ValueType = (FName)this.ValueType?.Clone();
     }
 }

UAssetAPI/UnrealTypes/Objects/Engine/Niagara/FNiagaraDataInterfaceGPUParamInfo.cs
@@ -1,8 +1,21 @@
-﻿using System;
+using System;
 using UAssetAPI.CustomVersions;
-using UAssetAPI.UnrealTypes;
+using UAssetAPI.PropertyTypes.Objects;
+
 
 namespace UAssetAPI.UnrealTypes;
+
+public struct FNiagaraVariableCommonReference(AssetBinaryReader reader)
+{
+    public FName Name = reader.ReadFName();
+    public FPackageIndex Type = new FPackageIndex(reader);
+
+    public void Write(AssetBinaryWriter writer)
+    {
+        writer.Write(Name);
+        Type.Write(writer);
+    }
+}
 
 public class FNiagaraDataInterfaceGeneratedFunction
 {
@@ -15,17 +28,25 @@ public class FNiagaraDataInterfaceGeneratedFunction
     /** Specifier values for this instance. */
     public (FName, FName)[] Specifiers;
 
+    public FNiagaraVariableCommonReference[] VariadicInputs = [];
+    public FNiagaraVariableCommonReference[] VariadicOutputs = [];
+    public ushort MiscUsageBitMask;
+
     public FNiagaraDataInterfaceGeneratedFunction(AssetBinaryReader reader)
     {
         DefinitionName = reader.ReadFName();
         InstanceName = reader.ReadFString();
 
-        int num = reader.ReadInt32();
-        Specifiers = new (FName, FName)[num];
-        for (int i = 0; i < num; i++)
+        Specifiers = reader.ReadArray(() => (reader.ReadFName(), reader.ReadFName()));
+
+        if (reader.Asset.GetCustomVersion<FNiagaraCustomVersion>() >= FNiagaraCustomVersion.AddVariadicParametersToGPUFunctionInfo)
         {
-            Specifiers[i] = (reader.ReadFName(), reader.ReadFName());
+            VariadicInputs = reader.ReadArray(() => new FNiagaraVariableCommonReference(reader));
+            VariadicOutputs = reader.ReadArray(() => new FNiagaraVariableCommonReference(reader));
         }
+
+        if (reader.Asset.GetCustomVersion<FNiagaraCustomVersion>() >= FNiagaraCustomVersion.SerializeUsageBitMaskToGPUFunctionInfo)
+            MiscUsageBitMask = reader.ReadUInt16();
     }
 
     public void Write(AssetBinaryWriter writer)
@@ -39,10 +60,28 @@ public class FNiagaraDataInterfaceGeneratedFunction
             writer.Write(spec.Item1);
             writer.Write(spec.Item2);
         }
+
+        if (writer.Asset.GetCustomVersion<FNiagaraCustomVersion>() >= FNiagaraCustomVersion.AddVariadicParametersToGPUFunctionInfo)
+        {
+            writer.Write(VariadicInputs.Length);
+            foreach (var input in VariadicInputs)
+            {
+                input.Write(writer);
+            }
+
+            writer.Write(VariadicOutputs.Length);
+            foreach (var output in VariadicOutputs)
+            {
+                output.Write(writer);
+            }
+        }
+
+        if (writer.Asset.GetCustomVersion<FNiagaraCustomVersion>() >= FNiagaraCustomVersion.SerializeUsageBitMaskToGPUFunctionInfo)
+            writer.Write(MiscUsageBitMask);
     }
 }
 
-public class FNiagaraDataInterfaceGPUParamInfo
+public class FNiagaraDataInterfaceGPUParamInfo : IStruct<FNiagaraDataInterfaceGPUParamInfo>
 {
     /** Symbol of this DI in the hlsl. Used for binding parameters. */
     public FString DataInterfaceHLSLSymbol;
@@ -53,10 +92,7 @@ public class FNiagaraDataInterfaceGPUParamInfo
     /** Information about all the functions generated by the translator for this data interface. */
     public FNiagaraDataInterfaceGeneratedFunction[] GeneratedFunctions = [];
 
-    public FNiagaraDataInterfaceGPUParamInfo()
-    {
-
-    }
+    public FNiagaraDataInterfaceGPUParamInfo() { }
 
     public FNiagaraDataInterfaceGPUParamInfo(AssetBinaryReader reader)
     {
@@ -65,14 +101,11 @@ public class FNiagaraDataInterfaceGPUParamInfo
 
         if (reader.Asset.GetCustomVersion<FNiagaraCustomVersion>() >= FNiagaraCustomVersion.AddGeneratedFunctionsToGPUParamInfo)
         {
-            int num = reader.ReadInt32();
-            GeneratedFunctions = new FNiagaraDataInterfaceGeneratedFunction[num];
-            for (int i = 0; i < num; i++)
-            {
-                GeneratedFunctions[i] = new FNiagaraDataInterfaceGeneratedFunction(reader);
-            }
+            GeneratedFunctions = reader.ReadArray(() => new FNiagaraDataInterfaceGeneratedFunction(reader));
         }
     }
+
+    public static FNiagaraDataInterfaceGPUParamInfo Read(AssetBinaryReader reader) => new FNiagaraDataInterfaceGPUParamInfo(reader);
 
     public int Write(AssetBinaryWriter writer)
     {
@@ -81,7 +114,6 @@ public class FNiagaraDataInterfaceGPUParamInfo
         writer.Write(DIClassName);
         if (writer.Asset.GetCustomVersion<FNiagaraCustomVersion>() >= FNiagaraCustomVersion.AddGeneratedFunctionsToGPUParamInfo)
         {
-            if (GeneratedFunctions == null) GeneratedFunctions = Array.Empty<FNiagaraDataInterfaceGeneratedFunction>();
             writer.Write(GeneratedFunctions.Length);
             foreach (var func in GeneratedFunctions)
             {
@@ -90,5 +122,10 @@ public class FNiagaraDataInterfaceGPUParamInfo
         }
 
         return (int)(writer.BaseStream.Position - offset);
+    }
+
+    public static FNiagaraDataInterfaceGPUParamInfo FromString(string[] d, UAsset asset)
+    {
+        throw new NotImplementedException();
     }
 }

commit 6de32a3953a0ac0dd4dc7150723d53a8ec5bb70f

Fixed unversioned map property serialization bug introduced in 5f715e7

UAssetAPI/PropertyTypes/Objects/MapPropertyData.cs
@@ -59,7 +59,7 @@ public class MapPropertyData : PropertyData
             case "StructProperty":
                 FName strucType = null;
                 FPropertyTypeName propertyTypeNameLocal = PropertyTypeName?.GetParameter(isKey ? 0 : 1);
-                if (reader.Asset.ObjectVersionUE5 >= ObjectVersionUE5.PROPERTY_TAG_COMPLETE_TYPE_NAME)
+                if (!reader.Asset.HasUnversionedProperties && reader.Asset.ObjectVersionUE5 >= ObjectVersionUE5.PROPERTY_TAG_COMPLETE_TYPE_NAME)
                 {
                     strucType = propertyTypeNameLocal?.GetParameter(0).GetName();
                 }

UAssetAPI/UnrealTypes/Objects/Engine/Niagara/FNiagaraDataInterfaceGPUParamInfo.cs
@@ -2,7 +2,6 @@ using System;
 using UAssetAPI.CustomVersions;
 using UAssetAPI.PropertyTypes.Objects;
 
-
 namespace UAssetAPI.UnrealTypes;
 
 public struct FNiagaraVariableCommonReference(AssetBinaryReader reader)
@@ -31,6 +30,8 @@ public class FNiagaraDataInterfaceGeneratedFunction
     public FNiagaraVariableCommonReference[] VariadicInputs = [];
     public FNiagaraVariableCommonReference[] VariadicOutputs = [];
     public ushort MiscUsageBitMask;
+
+    public FNiagaraDataInterfaceGeneratedFunction() { }
 
     public FNiagaraDataInterfaceGeneratedFunction(AssetBinaryReader reader)
     {